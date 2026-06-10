ae939df9bdcdc2731b71e215b4df105cd95b69a3
deveshsoniTWS <devesh.soni@thoughtswinsystems.com>
2026-06-10 20:56:10 +0530

    prisma connector code change

--- tejas-backend/src/lib/prisma.ts
+++ tejas-backend/src/lib/prisma.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaBetterSqlite3({
+  url: connectionString,
+});
+
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
